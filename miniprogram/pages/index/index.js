9dd0fdbf8ae950aa1c2b4f764d2cb6b90fe9c792
tangzhongyi <tangzhongyi@csvw.com>
2020-04-08 09:30:07 +0800

    add components & update index

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,8 +2,17 @@
 const app = getApp()
 
 Page({
-  data: {},
+  data: {
+    list: []
+  },
 
-  onLoad: function() {}
+  onLoad: function () {},
+  jump(i) {
+    console.log(i)
+    wx.navigateTo({url: 'pages/movie/list/list'})
+  },
+  tabChange(e) {
+    console.log('tab change', e)
+  }
 
 })
